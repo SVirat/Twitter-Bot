a49d139b488a07b3edbbaf8524ad0e9b5e33e2f9
Virat Singh <SVirat@users.noreply.github.com>
2017-08-19 23:36:13 +0530

    Update Credentials.py

--- Credentials.py
+++ Credentials.py
@@ -2,7 +2,7 @@
 Credentials for Twitter bot RobotBird, @ASCIIBird.
 @author Virat Singh, svirat@gmail.com
 """
-consumer_key = "Ly5qwf0IE6lj6djvpckXo7P2k"
-consumer_secret = "TwoVcWCjUKwQX2EBRYEzeDLgpbNMMkh1vgpNxYsVLi2hU4a9fG"
-access_token = "898187337422610432-LQPrraQcySHuWAYjg4WgXvI53PlhKs5"
-access_token_secret = "KhbaeHvr3gHmQ2LH73ou8u8r2wkm7HJWsmrh9upmTFgwr"
+consumer_key = "your_consumer_key_here"
+consumer_secret = "your_consumer_secret"
+access_token = "your_access_token"
+access_token_secret = "your_access_token_secret"
