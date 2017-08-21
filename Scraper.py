7709cf2d6a7b2a97daa77b16d27a2f457baa90ab
Virat Singh <SVirat@users.noreply.github.com>
2017-08-21 13:47:30 +0530

    Update Scraper Stocks
    Added the ability to pick up stock infromation.

--- Scraper.py
+++ Scraper.py
@@ -39,3 +39,34 @@ def get_free_book():
     book = soup.find("p", class_="name")
     author = soup.find("p", class_="author")
     return [book.get_text(), author.get_text()]
+
+def get_stocks():
+    """
+    Checks 9 stocks and gives their name, price, change and %change.
+    :return: list of 9 stocks, with their information
+    """
+    url = "http://www.money.cnn.com/data/us_markets/"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    stocks = soup.find_all('td', class_="wsod_aRight")
+    stocks = chunks(stocks, 3)
+    returning_stocks = []
+    for stock in stocks:
+        for item in stock:
+            name = str(item.span)
+            name = (name.split("streamfeed=")[-1].split(">")[0]).strip("\"")
+            if name not in returning_stocks:
+                returning_stocks.append(name)
+            returning_stocks.append(item.text)
+    returning_stocks = list(chunks(returning_stocks, 4))
+    return returning_stocks[:9]
+
+def chunks(list, num):
+    """
+    Helper function that splits a list into num-evenly sized chunks
+    :param list: the list to split
+    :param num: the number of sublists to make
+    :return: a list of the sublists
+    """
+    for i in range(0, len(list), num):
+        yield list[i:i + num]
